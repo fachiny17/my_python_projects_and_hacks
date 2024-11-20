e1f5261411780cf0456232878b3b70e29873bdd8
Iheanyi, Favour Chisom <iheanyifavour.ube3room312@gmail.com>
2024-11-20 15:43:56 +0100

    made an update

--- yt_video_download.py
+++ yt_video_download.py
@@ -6,17 +6,18 @@ url = input("Enter the video URL: ").strip()
 
 try:
     # Basic download options
-    # Download the best quality video available
-    ydl_options = {'format': 'best'}
 
     # Options to include subtitles
-    # ydl_options = {
-    #     'format': 'best',                # Download the best quality video
-    #     'writesubtitles': True,          # Download subtitles if available
-    #     'subtitleslangs': ['en'],        # Preferred subtitle language (e.g., 'en' for English)
-    #     'subtitlesformat': 'srt',        # Subtitle format (e.g., 'srt' or 'vtt')
-    #     'outtmpl': '%(title)s.%(ext)s'   # Save video with its title as the filename
-    # }
+    ydl_options = {
+        'format': 'best',                # Download the best quality video
+        'writesubtitles': True,          # Download subtitles if available
+        # Preferred subtitle language (e.g., 'en' for English)
+        'subtitleslangs': ['en'],
+        # Subtitle format (e.g., 'srt' or 'vtt')
+        'subtitlesformat': 'srt',
+        # Save video with its title as the filename
+        'outtmpl': '%(title)s.%(ext)s'
+    }
 
     # Create a downloader object and download the video
     with yt_dlp.YoutubeDL(ydl_options) as downloader:
